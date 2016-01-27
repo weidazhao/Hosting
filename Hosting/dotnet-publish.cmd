b0a846d9f977b5c374be5fab1ceddc191c44dad3
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-01-27 11:46:43 -0800

    Skip restore if there is already project.lock.json. Also copy appsettings.json and hosting.json to bin.

--- Hosting/dotnet-publish.cmd
+++ Hosting/dotnet-publish.cmd
@@ -2,21 +2,35 @@ set CURRENT_FOLDER=%~dp0
 set CONFIGURATION=Debug
 set FRAMEWORK=dnx451
 
-dotnet restore %CURRENT_FOLDER%..\Microsoft.ServiceFabric.AspNetCore\
+if not exist %CURRENT_FOLDER%..\Microsoft.ServiceFabric.AspNetCore\project.lock.json (
+    dotnet restore %CURRENT_FOLDER%..\Microsoft.ServiceFabric.AspNetCore\
+)
 
-dotnet restore %CURRENT_FOLDER%..\Counter\
+if not exist %CURRENT_FOLDER%..\Counter\project.lock.json (
+    dotnet restore %CURRENT_FOLDER%..\Counter\
+)
 dotnet publish %CURRENT_FOLDER%..\Counter\ -c %CONFIGURATION% -f %FRAMEWORK%
 robocopy /MIR %CURRENT_FOLDER%..\Counter\bin\%CONFIGURATION%\%FRAMEWORK%\ %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Counter\Code\
+copy /Y %CURRENT_FOLDER%..\Counter\appsettings.json %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Counter\Code\
+copy /Y %CURRENT_FOLDER%..\Counter\hosting.json %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Counter\Code\
 copy /Y %CURRENT_FOLDER%..\Counter\PackageRoot\ServiceManifest.xml %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Counter\
 
-dotnet restore %CURRENT_FOLDER%..\Gateway\
+if not exist %CURRENT_FOLDER%..\Gateway\project.lock.json (
+    dotnet restore %CURRENT_FOLDER%..\Gateway\
+)
 dotnet publish %CURRENT_FOLDER%..\Gateway\ -c %CONFIGURATION% -f %FRAMEWORK%
 robocopy /MIR %CURRENT_FOLDER%..\Gateway\bin\%CONFIGURATION%\%FRAMEWORK%\ %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Gateway\Code\
+copy /Y %CURRENT_FOLDER%..\Gateway\appsettings.json %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Gateway\Code\
+copy /Y %CURRENT_FOLDER%..\Gateway\hosting.json %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Gateway\Code\
 copy /Y %CURRENT_FOLDER%..\Gateway\PackageRoot\ServiceManifest.xml %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Gateway\
 
-dotnet restore %CURRENT_FOLDER%..\Sms\
+if not exist %CURRENT_FOLDER%..\Sms\project.lock.json (
+    dotnet restore %CURRENT_FOLDER%..\Sms\
+)
 dotnet publish %CURRENT_FOLDER%..\Sms\ -c %CONFIGURATION% -f %FRAMEWORK%
 robocopy /MIR %CURRENT_FOLDER%..\Sms\bin\%CONFIGURATION%\%FRAMEWORK%\ %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Sms\Code\
+copy /Y %CURRENT_FOLDER%..\Sms\appsettings.json %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Sms\Code\
+copy /Y %CURRENT_FOLDER%..\Sms\hosting.json %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Sms\Code\
 copy /Y %CURRENT_FOLDER%..\Sms\PackageRoot\ServiceManifest.xml %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Sms\
 
 copy /Y %CURRENT_FOLDER%ApplicationManifest.xml %CURRENT_FOLDER%\pkg\%CONFIGURATION%\
