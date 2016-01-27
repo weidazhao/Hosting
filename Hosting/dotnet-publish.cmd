3829933d3b6485b381c77f0cc7e0e14a4495e4cd
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-01-27 11:27:07 -0800

    restore packages

--- Hosting/dotnet-publish.cmd
+++ Hosting/dotnet-publish.cmd
@@ -2,14 +2,19 @@ set CURRENT_FOLDER=%~dp0
 set CONFIGURATION=Debug
 set FRAMEWORK=dnx451
 
+dotnet restore %CURRENT_FOLDER%..\Microsoft.ServiceFabric.AspNetCore\
+
+dotnet restore %CURRENT_FOLDER%..\Counter\
 dotnet publish %CURRENT_FOLDER%..\Counter\ -c %CONFIGURATION% -f %FRAMEWORK%
 robocopy /MIR %CURRENT_FOLDER%..\Counter\bin\%CONFIGURATION%\%FRAMEWORK%\ %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Counter\Code\
 copy /Y %CURRENT_FOLDER%..\Counter\PackageRoot\ServiceManifest.xml %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Counter\
 
+dotnet restore %CURRENT_FOLDER%..\Gateway\
 dotnet publish %CURRENT_FOLDER%..\Gateway\ -c %CONFIGURATION% -f %FRAMEWORK%
 robocopy /MIR %CURRENT_FOLDER%..\Gateway\bin\%CONFIGURATION%\%FRAMEWORK%\ %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Gateway\Code\
 copy /Y %CURRENT_FOLDER%..\Gateway\PackageRoot\ServiceManifest.xml %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Gateway\
 
+dotnet restore %CURRENT_FOLDER%..\Sms\
 dotnet publish %CURRENT_FOLDER%..\Sms\ -c %CONFIGURATION% -f %FRAMEWORK%
 robocopy /MIR %CURRENT_FOLDER%..\Sms\bin\%CONFIGURATION%\%FRAMEWORK%\ %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Sms\Code\
 copy /Y %CURRENT_FOLDER%..\Sms\PackageRoot\ServiceManifest.xml %CURRENT_FOLDER%\pkg\%CONFIGURATION%\Sms\
