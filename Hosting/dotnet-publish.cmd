7bff2fb4b5bb6e3a34ee4490c4a5f5b45f57e534
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-01-28 09:36:35 -0800

    update DotNet CLI to 1.0.1.001134

--- Hosting/dotnet-publish.cmd
+++ Hosting/dotnet-publish.cmd
@@ -1,5 +1,12 @@
+@echo off
+
+rem DotNet CLI Version
+rem d53b405ebd451b537636c7fcafa8697b69c85cf2
+rem 1.0.1.001134
+
 set CONFIGURATION=Debug
-set FRAMEWORK=dnx451
+set FRAMEWORK=net451
+set RUNTIME=win7-x64
 set SOLUTION_FOLDER=%~dp0..
 set PACKAGE_ROOT=%SOLUTION_FOLDER%\Hosting\pkg\%CONFIGURATION%
 
@@ -10,16 +17,18 @@ if exist %PACKAGE_ROOT%\ (
 if not exist %SOLUTION_FOLDER%\Microsoft.ServiceFabric.AspNetCore\project.lock.json (
     dotnet restore %SOLUTION_FOLDER%\Microsoft.ServiceFabric.AspNetCore\
 )
+dotnet compile %SOLUTION_FOLDER%\Microsoft.ServiceFabric.AspNetCore\ -c %CONFIGURATION% -f %FRAMEWORK%
 
 for %%S in (Counter,Gateway,Sms) do (
     if not exist %SOLUTION_FOLDER%\%%S\project.lock.json (
         dotnet restore %SOLUTION_FOLDER%\%%S\
     )
 
-    dotnet publish %SOLUTION_FOLDER%\%%S\ -c %CONFIGURATION% -f %FRAMEWORK%
+    dotnet compile %SOLUTION_FOLDER%\%%S\ -c %CONFIGURATION% -f %FRAMEWORK%
+    dotnet publish %SOLUTION_FOLDER%\%%S\ -c %CONFIGURATION% -f %FRAMEWORK% -r %RUNTIME%
 
     robocopy /E %SOLUTION_FOLDER%\%%S\PackageRoot\ %PACKAGE_ROOT%\%%S\
-    robocopy /E %SOLUTION_FOLDER%\%%S\bin\%CONFIGURATION%\%FRAMEWORK%\ %PACKAGE_ROOT%\%%S\Code\
+    robocopy /E %SOLUTION_FOLDER%\%%S\bin\%CONFIGURATION%\%FRAMEWORK%\%RUNTIME%\ %PACKAGE_ROOT%\%%S\Code\
     
     copy /Y %SOLUTION_FOLDER%\%%S\appsettings.json %PACKAGE_ROOT%\%%S\Code\
     copy /Y %SOLUTION_FOLDER%\%%S\hosting.json %PACKAGE_ROOT%\%%S\Code\
