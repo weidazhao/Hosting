fe6094f34242292efd6cd73477d3d94abc09b02e
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-02-03 17:10:43 -0800

    Update dotnet-publish.cmd

--- Hosting/dotnet-publish.cmd
+++ Hosting/dotnet-publish.cmd
@@ -14,10 +14,15 @@ if exist %PACKAGE_ROOT%\ (
     rmdir /q /s %PACKAGE_ROOT%\
 )
 
-if not exist %SOLUTION_FOLDER%\Microsoft.ServiceFabric.AspNetCore\project.lock.json (
-    dotnet restore %SOLUTION_FOLDER%\Microsoft.ServiceFabric.AspNetCore\
+if not exist %SOLUTION_FOLDER%\Microsoft.ServiceFabric.AspNetCore.Hosting\project.lock.json (
+    dotnet restore %SOLUTION_FOLDER%\Microsoft.ServiceFabric.AspNetCore.Hosting\
 )
-dotnet compile %SOLUTION_FOLDER%\Microsoft.ServiceFabric.AspNetCore\ -c %CONFIGURATION% -f %FRAMEWORK%
+dotnet compile %SOLUTION_FOLDER%\Microsoft.ServiceFabric.AspNetCore.Hosting\ -c %CONFIGURATION% -f %FRAMEWORK%
+
+if not exist %SOLUTION_FOLDER%\Microsoft.ServiceFabric.AspNetCore.Gateway\project.lock.json (
+    dotnet restore %SOLUTION_FOLDER%\Microsoft.ServiceFabric.AspNetCore.Gateway\
+)
+dotnet compile %SOLUTION_FOLDER%\Microsoft.ServiceFabric.AspNetCore.Gateway\ -c %CONFIGURATION% -f %FRAMEWORK%
 
 for %%S in (Counter,Gateway,Sms) do (
     if not exist %SOLUTION_FOLDER%\%%S\project.lock.json (
