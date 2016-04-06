87808b2d1172c1ad63899a55f384cded4c4ed47d
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-04-06 16:26:11 -0700

    Adds AddHttpRequestDispatcherProvider()

--- Microsoft.ServiceFabric.AspNetCore.Gateway/ServiceCollectionExtensions.cs
+++ Microsoft.ServiceFabric.AspNetCore.Gateway/ServiceCollectionExtensions.cs
@@ -5,6 +5,23 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddHttpRequestDispatcherProvider(this IServiceCollection services, HttpRequestDispatcherProvider provider)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (provider == null)
+            {
+                throw new ArgumentNullException(nameof(provider));
+            }
+
+            services.AddSingleton(provider);
+
+            return services;
+        }
+
         public static IServiceCollection AddDefaultHttpRequestDispatcherProvider(this IServiceCollection services)
         {
             if (services == null)
@@ -12,7 +29,7 @@ namespace Microsoft.Extensions.DependencyInjection
                 throw new ArgumentNullException(nameof(services));
             }
 
-            services.AddSingleton(_ => new HttpRequestDispatcherProvider(null, new[] { new AlwaysTreatedAsNonTransientExceptionHandler() }, null));
+            services.AddHttpRequestDispatcherProvider(new HttpRequestDispatcherProvider(null, new[] { new AlwaysTreatedAsNonTransientExceptionHandler() }, null));
 
             return services;
         }
