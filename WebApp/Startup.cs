699f3e4fa7492c7b76de64105ffd01b6d603ca61
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-06-25 18:20:25 -0700

    Add WebAppService to DI

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -23,6 +23,9 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add WebAppService.
+            services.AddServiceFabricService<WebAppService>();
+
             // Add framework services.
             services.AddMvc();
         }
