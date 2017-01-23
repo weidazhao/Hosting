241881eb894ef6ff2b981d0cdaff19794087d2af
David Zhao (DEVDIV) <dazhao@microsoft.com>
2017-01-22 17:29:48 -0800

    Adds WebApp

---
+++ WebApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.ServiceFabric.AspNetCore.Hosting;
+using Microsoft.ServiceFabric.Services.Runtime;
+using System.IO;
+
+namespace WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var communicationContext = CreateAspNetCoreCommunicationContext();
+
+            ServiceRuntime.RegisterServiceAsync("WebAppType", serviceContext => new WebAppService(serviceContext, communicationContext)).GetAwaiter().GetResult();
+
+            communicationContext.WebHost.Run();
+        }
+
+        private static AspNetCoreCommunicationContext CreateAspNetCoreCommunicationContext()
+        {
+            var webHost = new WebHostBuilder().UseKestrel()
+                                              .UseContentRoot(Directory.GetCurrentDirectory())
+                                              .UseStartup<Startup>()
+                                              .UseServiceFabricEndpoint("WebAppTypeEndpoint")
+                                              .Build();
+
+            return new AspNetCoreCommunicationContext(webHost);
+        }
+    }
+}
