10eea3e3dbcdf0cbfa292aff69012695f6229f94
David Zhao (DEVDIV) <dazhao@microsoft.com>
2017-01-22 17:23:33 -0800

    Adds Sms

---
+++ Sms/ISmsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Sms
+{
+    public interface ISmsService
+    {
+        Task<string> GetMessageAsync(string user);
+
+        Task PostMessageAsync(string user, string message);
+    }
+}
